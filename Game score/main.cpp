7ee02b40716b85c64116028e71260d067eb16ab7
20161104607 <a20161104607@mac26.local>
2018-07-07 18:45:19 +0800

    0
    0

--- Game score/main.cpp
+++ Game score/main.cpp
@@ -7,11 +7,71 @@
 //
 
 #include <iostream>
-#include <fstream>
+#include <cstring>
+#include <String>
+#include <stdio.h>
 #include <sstream>
-#include <string>
+#include <fstream>
 #include <vector>
 using namespace std;
+
+
+int menu_select()//菜单
+{
+    int MenuItem;
+    
+    printf("\n ");
+    printf("          | *********评分系统********* |           \n");
+    printf("          | ---------------------------------- |          \n");
+    printf("          |              主菜单项              |          \n");
+    printf("          | ---------------------------------- |          \n");
+    printf("          |	  1 ---  读取裁判信息  ---       |          	  \n");
+    printf("          |	  2 ---  裁判评分	 ---      |              \n");
+    printf("          |	  3 ---  读取学生信息	---       |              \n");
+    printf("          |	  4 ---  排序  ---       |              \n");
+    printf("          |	  5 ---  退出系统  ---     |              \n");
+    
+    do
+    {
+        printf("\n请输入选项（1－5）：");
+        fflush(stdin);
+        scanf("%d",&MenuItem);
+    }while(MenuItem<1||MenuItem>4);
+    
+    return MenuItem;
+}
+
+
+string Trim(string& str)
+{
+    str.erase(0,str.find_first_not_of(" \t\r\n"));
+    str.erase(str.find_last_not_of(" \t\r\n") + 1);
+    return str;
+}
+
+
+void input()
+{
+    int i,j,a=0,b=101,c=0;
+    double average;
+    
+    
+    cout<<"请输入选手成绩"<<endl;
+    for(i=1;i<=5;i++)
+    {
+        cout<<"第"<<i<<"位评委给分：";
+        cin>>j;
+        a=max(a,j);
+        b=min(b,j);
+        c=c+j;
+    }
+    average=(c-a-b)/5;
+    cout<<"减去一个最高分"<<a<<","<<"减去一个最低分"<<b<<endl;
+    cout<<"该选手的最后得分："<<average<<endl;
+    
+}
+
+
 int max (int x,int y);
 int min (int x,int y);
 
@@ -29,75 +89,97 @@ int max(int x, int y)
 
 int min(int x, int y)
 {
-    if(x>y)
-    {
-        return y;
-    }
-    else
-    {
-        return x;
-    }
-    return 0;
-}
-
-
-string Trim(string& str)
+if(x>y)
 {
-    str.erase(0,str.find_first_not_of(" \t\r\n"));
-    
-    str.erase(str.find_last_not_of(" \t\r\n") + 1);
-    
-    return str;
+    return y;
+}
+else
+{
+    return x;
+}
+return 0;
 }
 
+
+
+void loads()
+    {
+        
+        //int i,j,temp,score[8];
+        //float ave=0;
+        ifstream fin("/Users/a20161104607/Desktop/Game score/rater.csv");
+        
+        string line;
+        while (getline(fin, line))
+        {
+            //cout << line << endl;
+            
+            istringstream sin(line);
+            vector<string> fields;
+            string field;
+            while (getline(sin, field, ','))
+            {
+                fields.push_back(field);
+            }
+            string name = Trim(fields[0]);
+            string age = Trim(fields[1]);
+            string birthday = Trim(fields[2]);
+            cout << name << "\t" << age << "\t" << birthday << endl;
+        }
+        
+    }
+void student_information()
+{
+    
+    //int i,j,temp,score[8];
+    //float ave=0;
+    ifstream fin("/Users/a20161104607/Desktop/Game score/racer .csv");
+    
+    string line;
+    while (getline(fin, line))
+    {
+        //cout << line << endl;
+        
+        istringstream sin(line);
+        vector<string> fields;
+        string field;
+        while (getline(sin, field, ','))
+        {
+            fields.push_back(field);
+        }
+        string name = Trim(fields[0]);
+        string age = Trim(fields[1]);
+        string birthday = Trim(fields[2]);
+        cout << name << "\t" << age << "\t" << birthday << endl;
+    }
+    
+}
 
 
 
 int main()
 {
-    int a=0,b=101,c=0,i,j;
-    double average;
     
-    ifstream fin("/Users/a20161104607/Desktop/Game score/rater.csv"); //打开文件流操作
-        string line;
-        while (getline(fin, line))   //整行读取，换行符“\n”区分，遇到文件尾标志eof终止读取
+    while(1)
+    {
+        switch(menu_select())
         {
-            cout << line << endl; //整行输出
-            istringstream sin(line); //将整行字符串line读入到字符串流istringstream中
-            vector<string> fields; //声明一个字符串向量
-            string field;
-            while (getline(sin, field, ',')) //将字符串流sin中的字符读入到field字符串中，以逗号为分隔符
-            {
-                fields.push_back(field); //将刚刚读取的字符串添加到向量fields中
-            }
-            string name = Trim(fields[0]); //清除掉向量fields中第一个元素的无效字符，并赋值给变量name
-            string num = Trim(fields[1]); //清除掉向量fields中第二个元素的无效字符，并赋值给变量num
-            string sex= Trim(fields[2]); //清除掉向量fields中第三个元素的无效字符，并赋值给变量sex
-            cout <<"处理之后的字符串："<< name << "\t" << num << "\t" << sex << endl;
-            
+            case 1:
+                loads();
+                break;
+            case 2:
+                input();
+                break;
+              
+            case 3:
+                student_information();
+                break;
+            case 4:
+
+                printf("Thank you for using！\n");
+                return 0;
         }
-    
-    
-            cout<<"请输入选手成绩"<<endl;
-            for(i=1;i<=10;i++)
-            {
-                cout<<"第"<<i<<"位评委给分：";
-                cin>>j;
-                a=max(a,j);
-                b=min(b,j);
-                c=c+j;
-            }
-            average=(c-a-b)/8;
-            cout<<"减去一个最高分"<<a<<","<<"减去一个最低分"<<b<<endl;
-            cout<<"该选手的最后得分："<<average<<endl;
-    return 0;
-        }
+        
 
-
-            
-
-
-
-
-
-
+    }
+}
